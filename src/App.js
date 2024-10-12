dcb77f59863364e14bd65828fe2ffbeede50db4d
hassan zaker <h.zaker2222@gmail.com>
2024-10-11 22:55:40 -0400

    minor

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'https://api.movielads.net/';
+
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true
 
